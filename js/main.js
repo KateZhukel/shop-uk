d0452184be300babfecd1bf38654a79356247fa0
Kate Zhukel <zhukelkatya@gmail.com>
2019-11-11 09:16:42 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $(".mob-bl__header-m-btn--category").click(function(){
+        $(".mob-bl__header-m-btn-img--category").toggleClass("main");
+    });
+
+    $(".mob-bl__header-m-btn--type").click(function(){
+        $(".mob-bl__header-m-btn-img--type").toggleClass("main");
+    });
+});
