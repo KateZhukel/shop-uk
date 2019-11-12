8629e9325018403497541b6420bfddec4b4b26c0
Екатерина Жукель <E.Zhukel@webcom.lan>
2019-11-12 18:09:48 +0300

    tabs

--- js/main.js
+++ js/main.js
@@ -1,9 +1,30 @@
+
+/*Tabs*/
+function openTab(evt, tabName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("mob-bl__tab-content");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("mob-bl__tab-link");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+    console.log('test');
+}
+
 $(document).ready(function(){
-    $(".mob-bl__header-m-btn--category").click(function(){
-        $(".mob-bl__header-m-btn-img--category").toggleClass("main");
+    $(".mob-bl__header-check-btn--category").click(function(){
+        $(".mob-bl__header-check-btn-img--category").toggleClass("main");
     });
 
-    $(".mob-bl__header-m-btn--type").click(function(){
-        $(".mob-bl__header-m-btn-img--type").toggleClass("main");
+    $(".mob-bl__header-check-btn--type").click(function(){
+        $(".mob-bl__header-check-btn-img--type").toggleClass("main");
     });
+
 });
+
+
+
