2a50ce93f06669db2172768a402548d652337bf5
Екатерина Жукель <E.Zhukel@webcom.lan>
2019-11-13 18:14:58 +0300

    almost ready

--- js/main.js
+++ js/main.js
@@ -1,4 +1,18 @@
 
+/*Swiper-slider*/
+var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 2,
+    spaceBetween: 10,
+    slidesPerGroup: 1,
+    loop: false,
+    loopFillGroupWithBlank: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
+
+
 /*Tabs*/
 function openTab(evt, tabName) {
     var i, tabcontent, tablinks;
@@ -15,13 +29,25 @@ function openTab(evt, tabName) {
     console.log('test');
 }
 
+/*Clickable check*/
 $(document).ready(function(){
     $(".mob-bl__header-check-btn--category").click(function(){
         $(".mob-bl__header-check-btn-img--category").toggleClass("main");
+
     });
 
     $(".mob-bl__header-check-btn--type").click(function(){
         $(".mob-bl__header-check-btn-img--type").toggleClass("main");
+    });
+
+
+    $(".mob-bl__header-check-btn--category").click(function(){
+        $(".check-category").toggleClass("check-category--block");
+
+    });
+    $(".mob-bl__header-check-btn--type").click(function(){
+        $(".check-type").toggleClass("check-type--block");
+        console.log('test');
     });
 
 });
